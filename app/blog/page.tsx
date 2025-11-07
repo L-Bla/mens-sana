d409867fd4f285e1403e429990c06cea941fb425
L-Bla <lblaga00@fesb.hr>
2025-11-07 21:18:16 +0100

    Delete app directory

--- app/blog/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-    return <h1>Blog - Main page</h1>
-}
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home(){
-  return(
-    <main className="border-box flex min-h-screen flex-col items-center m-10">
-      <h1 className="text-2xl font-bold">MENS SANA</h1>
-    </main>
-  )
-}
